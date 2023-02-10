d7fb53c8980c51207aebd54c2642b7906cf12bc0
Srijidh <srijidh@oktohut.com>
2023-02-10 20:47:11 +0530

    completed Filteration

---
+++ includes/OKT_User_Table.php
@@ -0,0 +1,292 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class OKT_User_Table extends WP_List_Table {
+	public $filtered_users = array();
+
+	function __construct(){
+		global $status, $page;
+
+		//Set parent defaults
+		parent::__construct( array(
+			'singular'  => 'user',     //singular name of the listed records
+			'plural'    => 'users',    //plural name of the listed records
+			'ajax'      => false        //does this table support ajax?
+		) );
+	}
+
+	function column_default($item, $column_name){
+		switch($column_name){
+			case 'id':
+			case 'name':
+			case 'mobile':
+			case 'email':
+			case 'location':
+			case 'skill':
+			case 'experience':
+				return $item[$column_name];
+			default:
+				return print_r($item,true); //Show the whole array for troubleshooting purposes
+		}
+	}
+
+	function get_columns(){
+		$columns = array(
+//			'cb'        => '<input type="checkbox" />', //Render a checkbox instead of text
+			'id'     => 'ID',
+			'name'    => 'Name',
+			'mobile'  => 'Mobile',
+			'email'  => 'Email',
+			'location'     => 'Location',
+			'skill'    => 'Skill',
+			'experience'  => 'Experience',
+		);
+		return $columns;
+	}
+
+	function prepare_items() {
+		global $wpdb; //This is used only if making any database queries
+
+		/**
+		 * First, lets decide how many records per page to show
+		 */
+		$per_page = 25;
+
+
+		/**
+		 * REQUIRED. Now we need to define our column headers. This includes a complete
+		 * array of columns to be displayed (slugs & titles), a list of columns
+		 * to keep hidden, and a list of columns that are sortable. Each of these
+		 * can be defined in another method (as we've done here) before being
+		 * used to build the value for our _column_headers property.
+		 */
+		$columns = $this->get_columns();
+		$hidden = array();
+		$sortable = array();//$this->get_sortable_columns();
+
+
+		/**
+		 * REQUIRED. Finally, we build an array to be used by the class for column
+		 * headers. The $this->_column_headers property takes an array which contains
+		 * 3 other arrays. One for all columns, one for hidden columns, and one
+		 * for sortable columns.
+		 */
+		$this->_column_headers = array($columns, $hidden, $sortable);
+
+
+		/**
+		 * Instead of querying a database, we're going to fetch the example data
+		 * property we created for use in this plugin. This makes this example
+		 * package slightly different than one you might build on your own. In
+		 * this example, we'll be using array manipulation to sort and paginate
+		 * our data. In a real-world implementation, you will probably want to
+		 * use sort and pagination data to build a custom query instead, as you'll
+		 * be able to use your precisely-queried data immediately.
+		 */
+		$data = $this->filtered_users;
+
+
+		/**
+		 * This checks for sorting input and sorts the data in our array accordingly.
+		 *
+		 * In a real-world situation involving a database, you would probably want
+		 * to handle sorting by passing the 'orderby' and 'order' values directly
+		 * to a custom query. The returned data will be pre-sorted, and this array
+		 * sorting technique would be unnecessary.
+		 */
+//		function usort_reorder($a,$b){
+//			$orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'title'; //If no sort, default to title
+//			$order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
+//			$result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
+//			return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
+//		}
+//		usort($data, 'usort_reorder');
+
+
+		/***********************************************************************
+		 * ---------------------------------------------------------------------
+		 * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+		 *
+		 * In a real-world situation, this is where you would place your query.
+		 *
+		 * For information on making queries in WordPress, see this Codex entry:
+		 * http://codex.wordpress.org/Class_Reference/wpdb
+		 *
+		 * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		 * ---------------------------------------------------------------------
+		 **********************************************************************/
+		$args = $this->okt_build_args();
+		if ( $args ) {
+			$the_query = new WP_Query( $args );
+			if ( $the_query->have_posts() ) :
+				while ( $the_query->have_posts() ) :
+					$the_query->the_post();
+					$author_id      = get_post_field( 'post_author', get_the_ID() );
+					$user           = get_userdata( $author_id );
+					$first_name     = $user->user_firstname;
+					$mobile         = $user->user_nicename;
+					$email          = $user->user_email;
+					$location       = get_post_meta( get_the_ID(), '_candidate_location', true );
+					$skills_obj     = get_the_terms( get_the_ID(), 'resume_skill' );
+					$skill          = join( ', ', wp_list_pluck( $skills_obj, 'name' ) );
+					$experience_obj = get_the_terms( get_the_ID(), 'resume_experience' );
+					$experience     = join( ', ', wp_list_pluck( $experience_obj, 'name' ) );
+					$temp = array(
+						'id' => $author_id,
+						'name' => $first_name,
+						'mobile' => $mobile,
+						'email' => $email,
+						'location' => $location,
+						'skill' => $skill,
+						'experience' => $experience
+					);
+					array_push($this->filtered_users, $temp);
+				endwhile;
+				wp_reset_postdata();
+			endif;
+
+		}
+		$data = $this->filtered_users;
+
+		/**
+		 * REQUIRED for pagination. Let's figure out what page the user is currently
+		 * looking at. We'll need this later, so you should always include it in
+		 * your own package classes.
+		 */
+		$current_page = $this->get_pagenum();
+
+		/**
+		 * REQUIRED for pagination. Let's check how many items are in our data array.
+		 * In real-world use, this would be the total number of items in your database,
+		 * without filtering. We'll need this later, so you should always include it
+		 * in your own package classes.
+		 */
+		$total_items = count($data);
+
+
+		/**
+		 * The WP_List_Table class does not handle pagination for us, so we need
+		 * to ensure that the data is trimmed to only the current page. We can use
+		 * array_slice() to
+		 */
+//		$data = array_slice($data,(($current_page-1)*$per_page),$per_page);
+
+
+
+		/**
+		 * REQUIRED. Now we can add our *sorted* data to the items property, where
+		 * it can be used by the rest of the class.
+		 */
+		$this->items = $data;
+
+
+		/**
+		 * REQUIRED. We also have to register our pagination options & calculations.
+		 */
+		$this->set_pagination_args( array(
+			'total_items' => $total_items,                  //WE have to calculate the total number of items
+			'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
+			'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
+		) );
+	}
+
+	/**
+	 * Process Filter Form
+	 *
+	 * @return void
+	 */
+	public function oktufe_process_filter_form() {
+		$args = $this->okt_build_args();
+		if ( $args ) {
+			$the_query = new WP_Query( $args );
+			if ( $the_query->have_posts() ) :
+				while ( $the_query->have_posts() ) :
+					$the_query->the_post();
+					$author_id      = get_post_field( 'post_author', get_the_ID() );
+					$user           = get_userdata( $author_id );
+					$first_name     = $user->user_firstname;
+					$mobile         = $user->user_nicename;
+					$email          = $user->user_email;
+					$location       = get_post_meta( get_the_ID(), '_candidate_location', true );
+					$skills_obj     = get_the_terms( get_the_ID(), 'resume_skill' );
+					$skill          = join( ', ', wp_list_pluck( $skills_obj, 'name' ) );
+					$experience_obj = get_the_terms( get_the_ID(), 'resume_experience' );
+					$experience     = join( ', ', wp_list_pluck( $experience_obj, 'name' ) );
+					echo "UserId: " . $author_id . "<br />" .
+					     "Name: " . $first_name . "<br />" .
+					     "Mobile :" . $mobile . "<br />" .
+					     "Email: " . $email . "<br />" .
+					     "Location: " . $location . "<br />" .
+					     "Skill: " . $skill . "<br />" .
+					     "Experience: " . $experience . "<br/>";
+					echo "----------------------------<br />";
+					// TODO: get _candidate _location
+					//TODO: get skill
+					//TODO: get experience
+
+				endwhile;
+				wp_reset_postdata();
+			endif;
+
+		}
+	}
+
+	/**
+	 * Build Filter Query Arguments
+	 *
+	 * @return array
+	 */
+	private function okt_build_args() {
+		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		$args = array(
+			'post_type'      => 'resume',
+			'order'          => 'ASC',
+			'paged' => $paged,
+			'posts_per_page' => 25
+		);
+
+		if ( isset( $_GET['user-location'] ) && ! empty ( $_GET['user-location'] ) ) {
+			$args['meta_query'] = array(
+				array(
+					'key'     => '_candidate_location',
+					'compare' => 'LIKE',
+					'value'   => sanitize_key( $_GET['user-location'] ),
+				)
+			);
+		}
+
+		if ( ( isset( $_GET['user-skills'] ) && ! empty ( $_GET['user-skills'] ) ) ||
+		     ( isset( $_GET['user-experience'] ) && ! empty ( $_GET['user-experience'] ) )
+		) {
+			$args['tax_query'] = array(
+				'relation' => 'AND',
+			);
+			//resume_experience
+			//resume_skill
+			if ( isset( $_GET['user-skills'] ) && ! empty ( $_GET['user-skills'] ) ) {
+				$skill_query = array(
+					'taxonomy' => 'resume_skill',
+					'field'    => 'term_taxonomy_id',
+					'terms'    => [ intval( $_GET['user-skills'] ) ],
+					'operator' => 'IN'
+				);
+
+				array_push( $args['tax_query'], $skill_query );
+			}
+
+			if ( isset( $_GET['user-experience'] ) && ! empty ( $_GET['user-experience'] ) ) {
+				$exp_query = array(
+					'taxonomy' => 'resume_experience',
+					'field'    => 'term_taxonomy_id',
+					'terms'    => [ intval( $_GET['user-experience'] ) ],
+					'operator' => 'IN'
+				);
+
+				array_push( $args['tax_query'], $exp_query );
+			}
+		}
+
+		return $args;
+	}
+}
--- okt-wp-user-filter-export.php
+++ okt-wp-user-filter-export.php
@@ -1,30 +1,37 @@
 <?php
 /**
-* Plugin Name: User Filter and Exort
-* Plugin URI: https://wp-hut.com/
-* Description: Filter and export users
-* Version: 1.0.0
-* Requires at least: 5.2
-* Author: srijidh
-* Author URI: https://wp-hut.com/
-* License:  GPL v2 or later
-* License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Update URI:        https://wp-hut/user-filter-export-plugin/
-* Text Domain:       okt-ufe
-* Domain Path:       /languages
-**/
+ * Plugin Name: User Filter and Export
+ * Plugin URI: https://wp-hut.com/
+ * Description: Filter and export users
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Author: srijidh
+ * Author URI: https://wp-hut.com/
+ * License:  GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://wp-hut/user-filter-export-plugin/
+ * Text Domain:       okt-ufe
+ * Domain Path:       /languages
+ **/
 
-if( !defined( 'OKTUFE_VER' ) )
+if ( ! defined( 'OKTUFE_VER' ) ) {
 	define( 'OKTUFE_VER', '1.0.0' );
+}
 
+if ( ! class_exists( 'OKT_User_Table' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . '/includes/OKT_User_Table.php' );
+}
 
-class OKT_User_Filter_Export
-{
-    /**
+class OKT_User_Filter_Export {
+	/**
 	 * Static property to hold our singleton instance
 	 *
 	 */
 	static $instance = false;
+
+	public $plugin_name = 'User Filter and Export';
+
+	public $error_msg = '';
 
 	/**
 	 * This is our constructor
@@ -33,9 +40,10 @@ class OKT_User_Filter_Export
 	 */
 	private function __construct() {
 		// back end
-		add_action( 'plugins_loaded', array( $this, 'textdomain'));
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts'));
-		add_action('admin_menu', array( $this, 'main_menu'), 10);
+		add_action( 'plugins_loaded', array( $this, 'textdomain' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
+		add_action( 'admin_menu', array( $this, 'main_menu' ), 10 );
+//		add_action( 'admin_action_oktufe_form_response', array( $this, 'oktufe_filter_form_response' ) );
 	}
 
 	/**
@@ -45,17 +53,19 @@ class OKT_User_Filter_Export
 	 * @return WP_Comment_Notes
 	 */
 
-	 public static function getInstance() {
-		if ( !self::$instance )
+	public static function getInstance() {
+		if ( ! self::$instance ) {
 			self::$instance = new self;
+		}
+
 		return self::$instance;
 	}
 
-    public function textdomain() {
+	public function textdomain() {
 
-        load_plugin_textdomain( 'okt-ufe', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		load_plugin_textdomain( 'okt-ufe', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-    }
+	}
 
 	/**
 	 * Sub Menu Under Users Menu
@@ -63,14 +73,187 @@ class OKT_User_Filter_Export
 	 * @return void
 	 */
 	public function main_menu() {
-		add_submenu_page( 
-			'users.php', 
-			'User Filter Export', 
+		add_submenu_page(
+			'users.php',
 			'User Filter Export',
-			'manage_options', 
-			'okt-user-filter-export', 
-			array($this, 'oktufe_menu') 
-		   );
+			'User Filter Export',
+			'manage_options',
+			'okt-user-filter-export',
+			array( $this, 'oktufe_menu' )
+		);
+	}
+
+
+	/**
+	 * Error Notice
+	 *
+	 * @return void
+	 */
+	public function oktufe_error_notices() {
+		?>
+        <div class="notice notice-error">
+            <p><?php _e( $this->error_msg, 'okt-ufe' ); ?></p>
+        </div>
+		<?php
+	}
+
+	/**
+	 * Success Notice
+	 *
+	 * @return void
+	 */
+	public function oktufe_success_notices() {
+		?>
+        <div class="notice notice-success">
+            <p><?php _e( 'Operation completed successfully', 'okt-ufe' ); ?></p>
+        </div>
+		<?php
+	}
+
+	/**
+	 * Return Resume Taxonomy Terms
+	 *
+	 * @return int[]|string|string[]|WP_Error|WP_Term[]
+	 */
+	private function oktufe_get_taxonomy( $name ) {
+		return get_terms( array(
+			'taxonomy'   => $name,
+			'hide_empty' => true
+		) );
+	}
+
+	/**
+	 * Get Resume Locations
+	 *
+	 * @return string[]
+	 */
+	private function oktufe_get_locations() {
+		$args = array(
+			'post_type'      => 'resume',
+			'order'          => 'ASC',
+			'posts_per_page' => - 1
+//			'author'    => 4255
+		);
+
+		$locations = [];
+		$the_query = new WP_Query( $args );
+		if ( $the_query->have_posts() ) :
+			while ( $the_query->have_posts() ) :
+				$the_query->the_post();
+				$temp_location = get_post_meta( get_the_ID(), '_candidate_location', true );
+				if ( $temp_location ) {
+					$loc = explode( ',', $temp_location );
+					if ( $loc ) {
+						$temp_location = $loc[0];
+					}
+					if ( ! in_array( $temp_location, $locations ) ) {
+						$locations[] = $temp_location;
+					}
+				}
+			endwhile;
+			wp_reset_postdata();
+		endif;
+
+		return $locations;
+	}
+
+	/**
+	 * Display Filter Form
+	 *
+	 * @return String
+	 */
+	private function oktufe_get_filter_form() {
+		$experience                    = $this->oktufe_get_taxonomy( 'resume_experience' );
+		$skills                        = $this->oktufe_get_taxonomy( 'resume_skill' );
+		$locations                     = $this->oktufe_get_locations();
+		$oktufe_user_filter_meta_nonce = wp_create_nonce( 'oktufe_user_filter_form_nonce' );
+
+		$selected_skill      = '';
+		$selected_location   = '';
+		$selected_experience = '';
+
+		if ( isset( $_GET['user-location'] ) && ! empty ( $_GET['user-location'] ) ) {
+			$selected_location = $_GET['user-location'];
+		}
+		if ( isset( $_GET['user-experience'] ) && ! empty ( $_GET['user-experience'] ) ) {
+			$selected_experience = $_GET['user-experience'];
+		}
+		if ( isset( $_GET['user-skills'] ) && ! empty ( $_GET['user-skills'] ) ) {
+			$selected_skill = $_GET['user-skills'];
+		}
+
+		ob_start(); ?>
+        <form id="oktufe_user_filter_form" action="" method="get"
+              novalidate="novalidate">
+            <input type="hidden" name="page" value="okt-user-filter-export">
+            <!--                    <input type="hidden" name="action" value="oktufe_filter_form_response">-->
+            <input type="hidden" name="oktufe_meta_nonce"
+                   value="<?php echo $oktufe_user_filter_meta_nonce; ?>"/>
+            <table class="form-table" role="presentation">
+                <tbody>
+                <tr class="user-location-wrap">
+                    <th><label for="user-location">Location</label></th>
+                    <td>
+                        <select name="user-location" class="okt-select" id="user-location">
+                            <option value="">Select Location</option>
+							<?php foreach ( $locations as $location ): ?>
+								<?php if ( $selected_location == $location ): ?>
+                                    <option value="<?php echo esc_attr( $location ); ?>"
+                                            selected><?php echo $location; ?></option>
+								<?php else: ?>
+                                    <option value="<?php echo esc_attr( $location ); ?>"><?php echo $location; ?></option>
+								<?php endif; ?>
+							<?php endforeach; ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th><label for="user-skills">Skills</label></th>
+                    <td>
+                        <select name="user-skills" class="okt-select" id="user-skills">
+                            <option value="">Select Skill</option>
+							<?php
+							if ( ! empty( $skills ) && ! is_wp_error( $skills ) ):
+								foreach ( $skills as $skill ): ?>
+									<?php if ( $selected_skill == $skill->term_id ): ?>
+                                        <option value="<?php echo esc_attr( $skill->term_id ); ?>"
+                                                selected><?php echo $skill->name; ?></option>
+									<?php else: ?>
+                                        <option value="<?php echo esc_attr( $skill->term_id ); ?>"><?php echo $skill->name; ?></option>
+									<?php endif; ?>
+								<?php endforeach; endif; ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th><label for="user-experience">Experience</label></th>
+                    <td>
+                        <select name="user-experience" class="okt-select" id="user-experience">
+                            <option value="">Select Experience</option>
+							<?php
+							if ( ! empty( $experience ) && ! is_wp_error( $experience ) ):
+								foreach ( $experience as $exp ): ?>
+									<?php if ( $selected_experience == $exp->term_id ): ?>
+                                        <option value="<?php echo esc_attr( $exp->term_id ); ?>"
+                                                selected><?php echo $exp->name; ?></option>
+									<?php else: ?>
+                                        <option value="<?php echo esc_attr( $exp->term_id ); ?>"><?php echo $exp->name; ?></option>
+									<?php endif; ?>
+								<?php endforeach; endif; ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td>
+                        <input type="submit" name="submit" id="submit" class="button button-primary"
+                               value="Submit">
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </form>
+		<?php return ob_get_clean();
 	}
 
 	/**
@@ -78,20 +261,160 @@ class OKT_User_Filter_Export
 	 *
 	 * @return void
 	 */
-
 	public function oktufe_menu() {
-		echo "My Admin Page";
+		if ( current_user_can( 'edit_users' ) ):
+			echo $this->oktufe_get_filter_form();
+			if ( isset( $_GET['oktufe_meta_nonce'] ) && wp_verify_nonce( $_GET['oktufe_meta_nonce'], 'oktufe_user_filter_form_nonce' ) ) {
+				$user_table = new OKT_User_Table();
+				//Fetch, prepare, sort, and filter our data...
+				$user_table->prepare_items();
+				$user_table->display();
+			} else {
+				$this->error_msg = 'Invalid nonce';
+				add_action( 'admin_notices', array( $this, 'oktufe_error_notices' ) );
+			}
+//			$this->display_results();
+
+
+
+		else: ?>
+            <p> <?php __( "You are not authorized to perform this operation.", $this->plugin_name ); ?> </p>
+		<?php endif;
 	}
-    /**
+
+
+	/**
+	 * Display Result Table
+	 *
+	 * @return void
+	 */
+	private function display_results() {
+		if ( isset( $_GET['oktufe_meta_nonce'] ) && wp_verify_nonce( $_GET['oktufe_meta_nonce'], 'oktufe_user_filter_form_nonce' ) ) {
+			$this->oktufe_process_filter_form();
+
+		} else {
+			$this->error_msg = 'Invalid nonce';
+			add_action( 'admin_notices', array( $this, 'oktufe_error_notices' ) );
+		}
+	}
+
+	/**
+	 * Process Filter Form
+	 *
+	 * @return void
+	 */
+	public function oktufe_process_filter_form() {
+		$args = $this->okt_build_args();
+		if ( $args ) {
+			$the_query = new WP_Query( $args );
+			if ( $the_query->have_posts() ) :
+				while ( $the_query->have_posts() ) :
+					$the_query->the_post();
+					$author_id      = get_post_field( 'post_author', get_the_ID() );
+					$user           = get_userdata( $author_id );
+					$first_name     = $user->user_firstname;
+					$mobile         = $user->user_nicename;
+					$email          = $user->user_email;
+					$location       = get_post_meta( get_the_ID(), '_candidate_location', true );
+					$skills_obj     = get_the_terms( get_the_ID(), 'resume_skill' );
+					$skill          = join( ', ', wp_list_pluck( $skills_obj, 'name' ) );
+					$experience_obj = get_the_terms( get_the_ID(), 'resume_experience' );
+					$experience     = join( ', ', wp_list_pluck( $experience_obj, 'name' ) );
+					echo "UserId: " . $author_id . "<br />" .
+					     "Name: " . $first_name . "<br />" .
+					     "Mobile :" . $mobile . "<br />" .
+					     "Email: " . $email . "<br />" .
+					     "Location: " . $location . "<br />" .
+					     "Skill: " . $skill . "<br />" .
+					     "Experience: " . $experience . "<br/>";
+					echo "----------------------------<br />";
+					// TODO: get _candidate _location
+					//TODO: get skill
+					//TODO: get experience
+
+				endwhile;
+				wp_reset_postdata();
+			endif;
+
+		}
+	}
+
+	/**
+	 * Build Filter Query Arguments
+	 *
+	 * @return array
+	 */
+	private function okt_build_args() {
+		$args = array(
+			'post_type'      => 'resume',
+			'order'          => 'ASC',
+			'posts_per_page' => 25
+		);
+
+		if ( isset( $_GET['user-location'] ) && ! empty ( $_GET['user-location'] ) ) {
+			$args['meta_query'] = array(
+				array(
+					'key'     => '_candidate_location',
+					'compare' => 'LIKE',
+					'value'   => sanitize_key( $_GET['user-location'] ),
+				)
+			);
+		}
+
+		if ( ( isset( $_GET['user-skills'] ) && ! empty ( $_GET['user-skills'] ) ) ||
+		     ( isset( $_GET['user-experience'] ) && ! empty ( $_GET['user-experience'] ) )
+		) {
+			$args['tax_query'] = array(
+				'relation' => 'AND',
+			);
+			//resume_experience
+			//resume_skill
+			if ( isset( $_GET['user-skills'] ) && ! empty ( $_GET['user-skills'] ) ) {
+				$skill_query = array(
+					'taxonomy' => 'resume_skill',
+					'field'    => 'term_taxonomy_id',
+					'terms'    => [ intval( $_GET['user-skills'] ) ],
+					'operator' => 'IN'
+				);
+
+				array_push( $args['tax_query'], $skill_query );
+			}
+
+			if ( isset( $_GET['user-experience'] ) && ! empty ( $_GET['user-experience'] ) ) {
+				$exp_query = array(
+					'taxonomy' => 'resume_experience',
+					'field'    => 'term_taxonomy_id',
+					'terms'    => [ intval( $_GET['user-experience'] ) ],
+					'operator' => 'IN'
+				);
+
+				array_push( $args['tax_query'], $exp_query );
+			}
+		}
+
+		return $args;
+	}
+
+	/**
 	 * Admin styles and scripts
 	 *
 	 * @return void
 	 */
 
 	public function admin_scripts() {
-        // original version: OKTUFE_VER, replace time()
-		wp_enqueue_style( 'oktufe-admin-css', plugins_url('lib/css/admin.css', __FILE__), array(), time(), 'all' );
-        wp_enqueue_script( 'oktufe-admin-js', plugins_url('lib/js/admin.js', __FILE__), array('jquery'), time(), true );
+		// original version: OKTUFE_VER, replace time()
+
+
+		wp_register_style( 'select2css', '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css', false, '1.0', 'all' );
+		wp_register_script( 'select2', '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js', array( 'jquery' ), '1.0', true );
+		wp_enqueue_style( 'select2css' );
+		wp_enqueue_script( 'select2' );
+
+		wp_enqueue_style( 'oktufe-admin-css', plugins_url( 'lib/css/admin.css', __FILE__ ), array(), time(), 'all' );
+		wp_enqueue_script( 'oktufe-admin-js', plugins_url( 'lib/js/admin.js', __FILE__ ), array(
+			'jquery',
+			'select2'
+		), time(), true );
 	}
 }
 
